b57497d228b249a047b11281966caac2320bec26
Deanna <xander.deanna@gmail.com>
2021-05-11 18:13:52 -0500

    added service worker to index.html

---
+++ public/service-worker.js
@@ -0,0 +1,10 @@
+  const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "index.js",
+    "/db.js",
+    "/styles.css",
+    "/manifest.webmanifest",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+  ];
