cffcfb84d354bfb92999cdb03c93e3074ac80633
Deanna <xander.deanna@gmail.com>
2021-05-11 19:52:14 -0500

    added more to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-  const FILES_TO_CACHE = [
+const FILES_TO_CACHE = [
     "/",
     "/index.html",
     "index.js",
@@ -7,4 +7,37 @@
     "/manifest.webmanifest",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
-  ];
+];
+
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// install
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then((cache) => {
+                cache.addAll(FILES_TO_CACHE);
+                console.log("Your files were cached successfully!");
+            })
+            .then(self.skipWaiting())
+    );
+});
+
+// activate
+self.addEventListener("activate", function (evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== STATIC_CACHE && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+    self.clients.claim();
+  });
