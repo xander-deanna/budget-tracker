39f3b52d7957758258ad022a35db24b4caca9a3e
Deanna <xander.deanna@gmail.com>
2021-05-11 21:59:10 -0500

    finished creating service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -26,18 +26,48 @@ self.addEventListener('install', (event) => {
 });
 
 // activate
-self.addEventListener("activate", function (evt) {
-    evt.waitUntil(
-      caches.keys().then(keyList => {
-        return Promise.all(
-          keyList.map(key => {
-            if (key !== STATIC_CACHE && key !== DATA_CACHE_NAME) {
-              console.log("Removing old cache data", key);
-              return caches.delete(key);
-            }
-          })
-        );
-      })
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache.", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
     );
     self.clients.claim();
-  });
+});
+
+// fetch
+self.addEventListener("fetch", event => {
+    if (event.request.url.includes('/api/transaction')) {
+        console.log('[Service Worker] Fetch(data)', event.request.url);
+
+        event.respondWith(
+            caches.open(CACHE_NAME).then(cache => {
+                return fetch(event.request)
+                    .then(response => {
+                        if (response.status === 200) {
+                            cache.put(event.request.url, response.clone());
+                        }
+                        return response;
+                    })
+                    .catch(error => {
+                        return cache.match(event.request);
+                    });
+            }).catch(error => console.log(error))
+        );
+        return;
+    }
+    event.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+            return cache.match(event.request).then(response => {
+                return response || fetch(event.request);
+            });
+        })
+    );
+});
